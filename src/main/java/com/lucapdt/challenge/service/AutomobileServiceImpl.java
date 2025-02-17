87fb491bacc188d5ca67506e6e4ac8de7e3604bf
LucaPdt <pedottiluca@gmail.com>
2025-02-17 15:30:17 +0100

    feat-green: AutomobileService::findById
    - Implementato metodo: findById in AutomobileServiceImpl, restituisce correttamente l'automobile e solleva AutomobileNotFoundException se non trova una automobile per l'id inserito

--- src/main/java/com/lucapdt/challenge/service/AutomobileServiceImpl.java
+++ src/main/java/com/lucapdt/challenge/service/AutomobileServiceImpl.java
@@ -1,6 +1,9 @@
 package com.lucapdt.challenge.service;
 
+import com.lucapdt.challenge.exception.AutomobileNotFoundException;
 import com.lucapdt.challenge.model.entity.Automobile;
+import com.lucapdt.challenge.repository.AutomobileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -8,8 +11,16 @@ import java.util.Optional;
 @Service
 public class AutomobileServiceImpl implements AutomobileService{
 
+    @Autowired
+    private AutomobileRepository automobileRepository;
+
     @Override
     public Optional<Automobile> findById(int id) {
-        return Optional.empty();
+
+        Optional<Automobile> optionalAutomobile = automobileRepository.findById(id);
+        if(optionalAutomobile.isEmpty())
+            throw new AutomobileNotFoundException("Non e' stata trovata una automobile per l'id inserito");
+
+        return optionalAutomobile;
     }
 }
