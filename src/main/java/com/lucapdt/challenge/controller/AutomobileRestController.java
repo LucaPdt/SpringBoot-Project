f9a8772d0a96c728769a99975687e3b56b008b4b
LucaPdt <pedottiluca@gmail.com>
2025-02-18 15:28:06 +0100

    feat-green: AutomobileRestController::getAutomobileById
    - Implementato endpoint '/api/automobili/1', restituisce il DTO dell'Automobile con l'id specificato

--- src/main/java/com/lucapdt/challenge/controller/AutomobileRestController.java
+++ src/main/java/com/lucapdt/challenge/controller/AutomobileRestController.java
@@ -1,4 +1,34 @@
 package com.lucapdt.challenge.controller;
 
+import com.lucapdt.challenge.command.AutomobileCommand;
+import com.lucapdt.challenge.model.dto.AutomobileDTO;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class AutomobileRestController {
+
+    @Autowired
+    private AutomobileCommand automobileCommand;
+
+    @GetMapping("automobili/{id}")
+    public ResponseEntity<AutomobileDTO> getAutomobileById(@PathVariable int id) {
+//        ResponseEntity<AutomobileDTO> response;
+//
+//        try{
+//            response = ResponseEntity.ok(automobileCommand.findById(id));
+//        } catch(Exception e){
+//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//
+//        return  response;
+        return ResponseEntity.ok(automobileCommand.findById(id));
+    }
 }
