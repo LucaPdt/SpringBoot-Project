7f1376e0e29c112c8d4ce27b77174a292681ac8a
LucaPdt <pedottiluca@gmail.com>
2025-02-19 18:10:34 +0100

    fix: aggiunta annotazione @Component mancante a AuthenticationCommandImpl

--- src/main/java/com/lucapdt/challenge/command/AuthenticationCommandImpl.java
+++ src/main/java/com/lucapdt/challenge/command/AuthenticationCommandImpl.java
@@ -16,9 +16,11 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 
+@Component
 public class AuthenticationCommandImpl implements AuthenticationCommand{
 
     @Autowired
