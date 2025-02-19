248b17fbfcaa917a95b24dc10bdc3e928510f28e
LucaPdt <pedottiluca@gmail.com>
2025-02-19 17:45:31 +0100

    feat: AuthenticationCommand
    - Creata Interfaccia AuthenticationCommand
    - Creata classe AuthenticationCommandImpl che implementa i metodi:
        - login: effettua il login dell'utente
        - register: effettua la registrazione di un utente con ruolo di default "USER"

---
+++ src/main/java/com/lucapdt/challenge/command/AuthenticationCommand.java
@@ -0,0 +1,13 @@
+package com.lucapdt.challenge.command;
+
+import com.lucapdt.challenge.model.dto.AuthResponseDTO;
+import com.lucapdt.challenge.model.dto.LoginDTO;
+import com.lucapdt.challenge.model.dto.RegisterDTO;
+import org.springframework.http.ResponseEntity;
+
+public interface AuthenticationCommand {
+
+    AuthResponseDTO login(LoginDTO loginDto);
+
+    ResponseEntity<String> register(RegisterDTO registerDto);
+}
