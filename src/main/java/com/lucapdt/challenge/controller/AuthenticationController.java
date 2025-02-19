68c07f708f1f5d22bfbf0e116558bd115c95b54c
LucaPdt <pedottiluca@gmail.com>
2025-02-19 17:48:44 +0100

    feat: AuthenticationController
    - Creata classe AuthenticationController che gestisce gli endpoint
    '/api/auth' dedicati alla autenticazione di spring security
    - Creato metodo login che gestisce l'endpoint 'login' che permette ad un utente di autenticarsi
    - Creato metodo register che gestisce l'endpoint 'register' che permette di registrare un nuovo utente con ruolo "USER"

---
+++ src/main/java/com/lucapdt/challenge/controller/AuthenticationController.java
@@ -0,0 +1,31 @@
+package com.lucapdt.challenge.controller;
+
+import com.lucapdt.challenge.command.AuthenticationCommand;
+import com.lucapdt.challenge.model.dto.AuthResponseDTO;
+import com.lucapdt.challenge.model.dto.LoginDTO;
+import com.lucapdt.challenge.model.dto.RegisterDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationCommand authenticationCommand;
+
+    @PostMapping("login")
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginDTO loginDto){
+        return new ResponseEntity<>(authenticationCommand.login(loginDto), HttpStatus.OK);
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<String> register(@RequestBody RegisterDTO registerDto){
+        return authenticationCommand.register(registerDto);
+    }
+}
